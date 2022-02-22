b194c25af5370abca9b580ac6259fafa514cf277
Oleshkooo <olehchoma@gmail.com>
2022-02-22 17:45:16 +0200

    -

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './FontAwesome/icons/svg-with-js.min.css'
 import Game from './Components/Game'
 
 export default function App() {
+    console.log("Version - 0.0.1")
     return (
         <div>
             <Game />
